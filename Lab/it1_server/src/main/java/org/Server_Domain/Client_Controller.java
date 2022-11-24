b8b9c232f5cc299f506799d3a2aa2461dbbfa3c8
Gonzalogv23 <115221608+Gonzalogv23@users.noreply.github.com>
2022-11-24 19:44:27 +0100

    it1_server

---
+++ Lab/it1_server/src/main/java/org/Server_Domain/Client_Controller.java
@@ -0,0 +1,15 @@
+package Server_Domain;
+
+public class Client_Controller {
+
+	public void authenticate() {
+		// TODO - implement Client_Controller.authenticate
+		throw new UnsupportedOperationException();
+	}
+
+	public void register() {
+		// TODO - implement Client_Controller.register
+		throw new UnsupportedOperationException();
+	}
+
+}
